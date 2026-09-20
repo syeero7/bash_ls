e2d6d7b9c9b6da1defcabb1d532741401ecccec5
emal <emalhasitha@proton.me>
2026-09-20 12:24:48 +0000

    feat: add JSON-RPC decoding

--- src/jsonrpc.zig
+++ src/jsonrpc.zig
@@ -4,6 +4,7 @@ const testing = std.testing;
 const Allocator = std.mem.Allocator;
 
 const field_separator = [_]u8{ '\r', '\n', '\r', '\n' };
+const header_field_name = "Content-Length: ";
 
 const Message = struct {
     jsonrpc: []const u8 = "2.0",
@@ -15,8 +16,32 @@ const Message = struct {
 pub fn encode(allocator: Allocator, json_struct: anytype) ![]u8 {
     const content = try json.Stringify.valueAlloc(allocator, json_struct, .{});
     defer allocator.free(content);
-    const fmt = "Content-Length: {d}" ++ field_separator ++ "{s}";
+    const fmt = header_field_name ++ "{d}" ++ field_separator ++ "{s}";
     return std.fmt.allocPrint(allocator, fmt, .{ content.len, content });
+}
+
+pub fn decode(comptime T: type, reader: *std.Io.Reader, allocator: Allocator) !json.Parsed(T) {
+    const content = try getContent(reader, allocator);
+    defer allocator.free(content);
+    return try json.parseFromSlice(T, allocator, content, .{ .ignore_unknown_fields = true });
+}
+
+pub fn getContent(reader: *std.Io.Reader, allocator: Allocator) ![]u8 {
+    var alloc_writer = std.Io.Writer.Allocating.init(allocator);
+    defer alloc_writer.deinit();
+
+    _ = try reader.streamDelimiter(&alloc_writer.writer, '\n');
+    const line = alloc_writer.written();
+    if (!std.mem.endsWith(u8, line, "\r")) return error.SeparatorNotFound;
+    const separator_rest = try reader.takeArray(3);
+    if (!std.mem.eql(u8, separator_rest, "\n\r\n")) return error.SeparatorNotFound;
+
+    if (std.mem.cut(u8, line[0..(line.len - 1)], header_field_name)) |parts| {
+        const content_length = try std.fmt.parseInt(usize, parts.@"1", 10);
+        return try reader.readAlloc(allocator, content_length);
+    }
+
+    return error.ContentLengthNotFound;
 }
 
 test "encode json" {
@@ -25,5 +50,15 @@ test "encode json" {
     const result = try encode(allocator, msg);
     defer allocator.free(result);
 
-    try testing.expectEqualStrings(result, "Content-Length: 13" ++ field_separator ++ "{\"test\":true}");
+    try testing.expectEqualStrings(result, header_field_name ++ "13" ++ field_separator ++ "{\"test\":true}");
+}
+
+test "decode json" {
+    const allocator = testing.allocator;
+    var test_reader = std.Io.Reader.fixed(header_field_name ++ "13" ++ field_separator ++ "{\"test\":true}");
+    const test_struct = struct { @"test": bool = true };
+    const result = try decode(test_struct, &test_reader, allocator);
+    defer result.deinit();
+
+    try testing.expectEqual(result.value.@"test", true);
 }

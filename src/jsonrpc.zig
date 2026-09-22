305f0b4623e194c874b06d36549516491e2eb54b
emal <emalhasitha@proton.me>
2026-09-22 03:45:48 +0000

    feat: add base protocol types

--- src/jsonrpc.zig
+++ src/jsonrpc.zig
@@ -3,15 +3,91 @@ const json = std.json;
 const testing = std.testing;
 const Allocator = std.mem.Allocator;
 
+pub const Integer = i32;
+
+pub const UInteger = u32;
+
+pub const Decimal = f32;
+
+pub const String = []const u8;
+
+pub const LSPObject = std.StringHashMap(LSPAny);
+
+pub const LSPArray = std.ArrayList(LSPAny);
+
+pub const LSPAny = union(enum) {
+    lsp_array: LSPArray,
+    lsp_object: LSPObject,
+    string: String,
+    initeger: Integer,
+    uninteger: UInteger,
+    decimal: Decimal,
+    boolean: bool,
+    null: null,
+};
+
+pub const RequestMessage = struct {
+    jsonrpc: []const u8 = "2.0",
+    id: Integer = undefined,
+    method: String = undefined,
+    params: type = null,
+};
+
+pub const ResponseMessage = struct {
+    jsonrpc: []const u8 = "2.0",
+    id: Integer = undefined,
+    result: LSPAny = null,
+    @"error": ResponseError = null,
+};
+
+pub const Error = error{
+    ParseError,
+    InvalidRequest,
+    MethodNotFound,
+    InvalidParams,
+    InternalError,
+    ServerNotInitialized,
+    UnknownErrorCode,
+    RequestFailed,
+    ServerCancelled,
+    ContentModified,
+    RequestCancelled,
+};
+
+pub const ResponseError = struct {
+    code: Integer = undefined,
+    message: String = undefined,
+    data: LSPAny = null,
+
+    pub fn init(err: Error, msg: String, data: ?LSPAny) ResponseError {
+        return .{
+            .message = msg,
+            .data = data,
+            .code = switch (err) {
+                .ParseError => Integer(-32700),
+                .InvalidRequest => Integer(-32600),
+                .MethodNotFound => Integer(-32601),
+                .InvalidParams => Integer(-32602),
+                .InternalError => Integer(-32603),
+                .ServerNotInitialized => Integer(-32002),
+                .UnknownErrorCode => Integer(-32001),
+                .RequestFailed => Integer(-32803),
+                .ServerCancelled => Integer(-32802),
+                .ContentModified => Integer(-32801),
+                .RequestCancelled => Integer(-32800),
+            },
+        };
+    }
+};
+
+pub const NotificationMessage = struct {
+    jsonrpc: []const u8 = "2.0",
+    method: String = undefined,
+    params: type = null,
+};
+
 const field_separator = [_]u8{ '\r', '\n', '\r', '\n' };
 const header_field_name = "Content-Length: ";
-
-const Message = struct {
-    jsonrpc: []const u8 = "2.0",
-    id: u64 = undefined,
-    method: []const u8 = undefined,
-    // params :
-};
 
 pub fn encode(allocator: Allocator, json_struct: anytype) ![]u8 {
     const content = try json.Stringify.valueAlloc(allocator, json_struct, .{});
